4dc56272988bae7f94f90c1dc42b39faf04edb7e
asetriza <57208705+asetriza@users.noreply.github.com>
2020-09-08 01:52:18 +0600

    Fixed

--- src/ceaserscode.py
+++ src/ceaserscode.py
@@ -1,54 +1,51 @@
 import enchant
 
-letters = (
-    'abcdefghijklmnopqrstuvwxyz'
-    )
+letters = "abcdefghijklmnopqrstuvwxyz"
 letters = letters + letters
 
-enc_list = (
-    '--+-'
-)
+enc_list = "--+-"
 
 d = enchant.Dict("en_US")
 
+
 def encrypt():
 
-# Юзер инпут !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Юзер инпут !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     while True:
         enc = input('Enter "+" to encrypt, "-" to decrypt or "--" to decode: ').strip()
         enc_f = enc_list.find(enc)
         enc_l = len(enc)
         if enc_f == -1:
-            print('It is not + or -')
+            print("It is not + or -")
         elif enc_l == 0:
-            print('Entered nothing')
+            print("Entered nothing")
         else:
             break
 
-    if enc == '+':
-        message_user = 'Enter your message to encrypt: '
-    elif enc == '-':
-        message_user = 'Enter your message to decrypt: '
-    elif enc == '--':
-        message_user = 'Enter your message, try to decrypt : '
+    if enc == "+":
+        message_user = "Enter your message to encrypt: "
+    elif enc == "-":
+        message_user = "Enter your message to decrypt: "
+    elif enc == "--":
+        message_user = "Enter your message, try to decrypt : "
 
     message = input(message_user).strip()
 
-    if enc == '--':
+    if enc == "--":
         pass
     else:
         while True:
-            message_key = input('Enter a message key in range 1-25: ').strip()
+            message_key = input("Enter a message key in range 1-25: ").strip()
             if not message_key.isnumeric():
-                print('Enter a number not letter/s')
+                print("Enter a number not letter/s")
             elif not 1 <= int(message_key) <= 25:
-                print('Enter a number in a range 1-25: ')
+                print("Enter a number in a range 1-25: ")
             else:
                 break
 
-# Расшифровать !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    if enc == '--':
-        print('Try find yor message here: ')
+    # Расшифровать !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if enc == "--":
+        print("Try find yor message here: ")
         aa = []
         aaaa = []
 
@@ -66,7 +63,7 @@ def encrypt():
 
         for i in range(0, 23):
             aaa = []
-            decrypted_decrypted = ''
+            decrypted_decrypted = ""
             for aa1 in aa:
                 aaa.append(aa1[i])
             aaaa.append(aaa)
@@ -76,7 +73,9 @@ def encrypt():
                 if ai == -1:
                     decrypted_decrypted = decrypted_decrypted + message[inc]
                 if message[inc].isupper() is True:
-                    decrypted_decrypted = decrypted_decrypted.replace(decrypted_decrypted[-1], letters[ai].upper())
+                    decrypted_decrypted = decrypted_decrypted.replace(
+                        decrypted_decrypted[-1], letters[ai].upper()
+                    )
 
             chk_decode_a = []
             for decode in decrypted_decrypted.split():
@@ -92,28 +91,33 @@ def encrypt():
         else:
             pass
 
-# Кодинг, Декодинг !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    if enc == '--':
+    # Кодинг, Декодинг !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if enc == "--":
         pass
     else:
-        encrypted = ''
+        encrypted = ""
 
         for letter in message:
             position = letters.find(letter.lower())
 
             if position != -1:
-                position = position + int(f'{enc}{message_key}')
+                position = position + int(f"{enc}{message_key}")
             if position != -1:
                 encrypted = encrypted + letters[int(position)]
             if letter.isupper() is True:
-                encrypted = encrypted.replace(encrypted[-1], letters[int(position)].upper())
+                encrypted = encrypted.replace(
+                    encrypted[-1], letters[int(position)].upper()
+                )
+            if letter.isupper is True:
+                encrypted = encrypted.replace()
             if position == -1:
                 encrypted = encrypted + letter
 
         if enc == "+":
-            print('Encrypted: ' + encrypted)
+            print("Encrypted: " + encrypted)
         else:
-            print('Decrypted: ' + encrypted)
+            print("Decrypted: " + encrypted)
+
 
 if __name__ == "__main__":
     encrypt()
